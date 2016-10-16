fb8a521840ffa5bb8ac4c20a2b1acef8ad593347
DZCoderX <david@ruizhang.ca>
2016-10-16 11:07:59 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //configuration varible
+//gives the state of the game (either local host or server)
 var REST = window.location.href.slice(0, window.location.href.length - 1);
 var socket;
+//incase anything doesn't load
 window.onload = function(){
-    //Connect to socket io
+    //Connect to socket io (backend)
     establishConnection();
 };

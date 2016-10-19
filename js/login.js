fa1806e93293dacba30331b2113d1277fd99f2f5
Johnson Han <hancanada@hotmail.com>
2016-10-18 22:22:58 -0400

    Game & Login Boot up Order

--- js/login.js
+++ js/login.js
@@ -1,5 +1,7 @@
 function showLoginScreen(decision){
 	if(decision == true){
+		$('#body').css("background","url('img/background.jpg')");
+		$('#body').css("background-size","cover");
 		setTimeout(function(){
 			$('.login').addClass("login-active");
 			$(".login").css("opacity","1");
@@ -15,6 +17,7 @@ function showLoginScreen(decision){
 	}
 	// Hide login Component
 	$('.login').removeClass("login-active");
+	$('#body').css("background","");
 	$('.login').html('');
 	setTimeout(function(){$('.login').addClass("hidden")}, 400);
 }
@@ -31,6 +34,15 @@ function requestTostartGame(){
 function startGame(){
 	$("input").css("width","0%");
 	$("input").css("border","none");
-	setTimeout(function(){showLoginScreen(false);}, 400);
 	userLoggedIn = true;
+	setTimeout(function(){showLoginScreen(false);}, 400);
+	setTimeout(function(){
+		//Game
+		var game = new Phaser.Game(1200,500, Phaser.CANVAS,'');
+			game.state.add('Boot', Game.Boot);
+			game.state.add('Preloader', Game.Preloader);
+			game.state.add('MainMenu', Game.MainMenu);
+			game.state.add('Level1', Game.Level1)
+			game.state.start('Boot');
+	}, 600);
 }
--- js/script.js
+++ js/script.js
@@ -16,13 +16,6 @@ window.onload = function(){
         if(key == 13 && !userLoggedIn) $('.login-button').trigger( "click" );
 		if(!userLoggedIn && key != 13)$(".login-instruction").html('');
     }
-    //Game
-    var game = new Phaser.Game(1200,500, Phaser.CANVAS,'');
-        game.state.add('Boot', Game.Boot);
-        game.state.add('Preloader', Game.Preloader);
-        game.state.add('MainMenu', Game.MainMenu);
-        game.state.add('Level1', Game.Level1)
-        game.state.start('Boot');
 };
 
 //Monitor

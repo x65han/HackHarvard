47f3a3aaef01a1ba457108508119a8b4cf799941
DZCoderX <david@ruizhang.ca>
2016-10-23 00:14:38 -0400

    New Map

--- js/Preloader.js
+++ js/Preloader.js
@@ -15,7 +15,7 @@ Game.Preloader.prototype = {
 
         //Load all assets
         this.load.tilemap('map','img/tile.csv');
-        this.load.image('tileset', 'img/colors.png'); 
+        this.load.image('tileset', 'img/colors1.png'); 
         this.load.image('dot', 'img/coin.jpg');
         this.load.image('dot2', 'img/RainbowCoin.png');
         //Coin layer added
